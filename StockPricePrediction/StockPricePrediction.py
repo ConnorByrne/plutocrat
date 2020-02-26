36771d072c43be11b688072898ca93f3964ce2b3
Connor Byrne <connorlukebyrne@gmail.com>
2020-02-26 22:38:05 +0000

    changing all strings to numbers

--- StockPricePrediction/StockPricePrediction.py
+++ StockPricePrediction/StockPricePrediction.py
@@ -1,8 +1,8 @@
 import mysql.connector
 import tensorflow as td
 from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Dense
+#from keras.models import Sequential
+#from keras.layers import Dense
 import numpy as np
 import mysql.connector
 import pandas_datareader.data as web
@@ -14,7 +14,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-
+from string import ascii_lowercase
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -37,9 +37,21 @@ def getPrice(symbol,dateString):
         start = dt.datetime(int(dateInt[0]),int(dateInt[1]),int(dateInt[2]))
         end = date.today()
         df=web.get_data_yahoo(symbol,start=start,end=end)
-        return df.head(1)
+        return df.head(1).astype(str)
     except:
         return "Error"
+
+def alphabet_position(text):
+    LETTERS={letter: str(index) for index, letter in enumerate(ascii_lowercase, start=1)}
+    text=text.lower()
+    #numbers=[LETTERS[character] for character in text if character in LETTERS elif isDigit(character)]
+    numbers=[]
+    for character in text:
+        if character in LETTERS:
+            numbers.append(LETTERS[character])
+        elif character.isdigit():
+            numbers.append(character)
+    return int(''.join(numbers))
 
 myCursor = db.cursor()
 
@@ -47,14 +59,22 @@ myCursor.execute("SELECT* FROM company, sentiment, company_sentiment WHERE(compa
 companies=[]
 yahoo = []
 for x in myCursor:
-    
-    companies.append(x)
+    #companyid=x[0]
+    #name=x[1]
+    #symbol=x[2]
+    #sentimentid=x[3]
+    #sentimentdata=x[4]
+    #sentimentdate=x[5]
+    company = (x[0],alphabet_position(x[4]),alphabet_position(x[5]))
+    companies.append(company)
     dateString = (x[5])
     if(dateString[0]=="0"):
         dateString = "2"+dateString
     symbol=x[2]
-    yahoo.append((x[1],x[5],getPrice(symbol,dateString)))
-    print (x)
+    yaho=(x[0],alphabet_position(x[5]),alphabet_position(getPrice(symbol,dateString)))
+    yahoo.append(yaho)
+    print(company)
+    print(yaho)
     
 #trainCompanies = companies[:int(len(companies)*.8)]
 #testCompanies = companies[int(len(companies)*.8):]
@@ -79,10 +99,10 @@ yahooArray=ohe.fit_transform(yahooArray).toarray()
 
 x_train,x_test,y_train,y_test = train_test_split(companiesArray,yahooArray,test_size=0.1)
 
-model = Sequential()
-model.add(Dense(16, input_dim=20, activation='relu'))
-model.add(Dense(12, activation='relu'))
-model.add(Dense(4,activation='softmax'))
+model = keras.Sequential()
+model.add(keras.layers.Dense(16, input_dim=20, activation='relu'))
+model.add(keras.layers.Dense(12, activation='relu'))
+model.add(keras.layers.Dense(4,activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
@@ -96,7 +116,7 @@ for i in range(len(y_pred)):
 
 test=list()
 for i in range(len(y_test)):
-    test.append(np.atgmax(y_test[i]))
+    test.append(np.argmax(y_test[i]))
 
 a=accuracy_score(pred,test)
 print("Accuracy is:", a*100)
@@ -108,7 +128,7 @@ plt.plot(history.history['val_acc'])
 plt.title('Model accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc'upper left')
+plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 #model = keras.Sequential()
 #model.add(keras.layers.Flatten(input_shape=(len(companies),len(yahoo))))
