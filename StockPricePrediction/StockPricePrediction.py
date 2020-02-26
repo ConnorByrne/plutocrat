2d92e5b956314c48a0594854ddf6b53e316e9342
Connor Byrne <connorlukebyrne@gmail.com>
2020-02-26 20:30:57 +0000

    Rebuilt neural network after normalising data

--- StockPricePrediction/StockPricePrediction.py
+++ StockPricePrediction/StockPricePrediction.py
@@ -1,13 +1,19 @@
 import mysql.connector
 import tensorflow as td
 from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense
 import numpy as np
 import mysql.connector
 import pandas_datareader.data as web
 import datetime as dt
 from datetime import date
 import re
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 db = mysql.connector.connect(
     host="localhost",
@@ -50,31 +56,73 @@ for x in myCursor:
     yahoo.append((x[1],x[5],getPrice(symbol,dateString)))
     print (x)
     
-trainCompanies = companies[:int(len(companies)*.8)]
-testCompanies = companies[int(len(companies)*.8):]
+#trainCompanies = companies[:int(len(companies)*.8)]
+#testCompanies = companies[int(len(companies)*.8):]
 
-trainYahoo = yahoo[:int(len(yahoo)*.8)]
-testYahoo = yahoo[int(len(yahoo)*.8):]
+#trainYahoo = yahoo[:int(len(yahoo)*.8)]
+#testYahoo = yahoo[int(len(yahoo)*.8):]
 
-companiesArray = np.asarray(trainCompanies)
-yahooArray = np.asarray(trainYahoo)
+companiesArray = np.asarray(companies)
+yahooArray = np.asarray(yahoo)
 
-testYahooArray = np.asarray(testYahoo)
-testCompaniesArray = np.asarray(testYahoo)
+#testYahooArray = np.asarray(testYahoo)
+#testCompaniesArray = np.asarray(testYahoo)
 
-model = keras.Sequential()
-#model.add(keras.layers.Flatten(input_shap=(len(companies),len(yahoo))))
-model.add(keras.layers.Embedding(10000,16))
-model.add(keras.layers.GlobalAveragePooling1D())
-model.add(keras.layers.Dense(16, activation="relu"))
-model.add(keras.layers.Dense(1,activation="sigmoid"))
+sc=StandardScaler()
+yahooArray=sc.fit_transform(yahooArray)
+companiesArray=sc.fit_transform(companiesArray)
 
-model.summary()
-model.compile(optimizer = "adam", loss="mean_squared_error", metrics=["accuracy"])
+ohe=OneHotEncoder()
 
-model.fit(companiesArray,yahooArray,epochs=40)
+companiesArray=ohe.fit_transform(companiesArray).toarray()
+yahooArray=ohe.fit_transform(yahooArray).toarray()
+
+x_train,x_test,y_train,y_test = train_test_split(companiesArray,yahooArray,test_size=0.1)
+
+model = Sequential()
+model.add(Dense(16, input_dim=20, activation='relu'))
+model.add(Dense(12, activation='relu'))
+model.add(Dense(4,activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+history = model.fit(x_train,y_train,epochs=40,batch_size=64)
+
+y_pred = model.predict(x_test)
+pred=list()
+
+for i in range(len(y_pred)):
+    pred.append(np.argmax(y_pred[i]))
+
+test=list()
+for i in range(len(y_test)):
+    test.append(np.atgmax(y_test[i]))
+
+a=accuracy_score(pred,test)
+print("Accuracy is:", a*100)
+
+history = model.fit(x_train, y_train, validation_data=(x_test,y_test), epochs=100, batch_size=64)
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc'upper left')
+plt.show()
+#model = keras.Sequential()
+#model.add(keras.layers.Flatten(input_shape=(len(companies),len(yahoo))))
+#model.add(keras.layers.Embedding(10000,16))
+#model.add(keras.layers.GlobalAveragePooling1D())
+#model.add(keras.layers.Dense(16, activation="relu"))
+#model.add(keras.layers.Dense(1,activation="sigmoid"))
+
+#model.summary()
+#model.compile(optimizer = "adam", loss="mean_squared_error", metrics=["accuracy"])
+
+#model.fit(companiesArray,yahooArray,epochs=40)
 
 #predict = model.predict(testCompanies,testYahoo)
-test_acc = model.evaluate(testCompaniesArray,testYahooArray)
-print("tested accuracy:", test_acc)
+#test_acc = model.evaluate(testCompaniesArray,testYahooArray)
+#print("tested accuracy:", test_acc)
 
